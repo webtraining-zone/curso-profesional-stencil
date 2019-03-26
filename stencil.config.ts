5e43ca59164ab1047f7b877184b96e66d5028e06
Alex Arriaga <alexzander.arriaga@base22.com>
2019-03-25 19:51:33 -0600

    Agregar ejemplo de integración con SASS

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'mycomponent',
@@ -9,5 +10,12 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null // disable service workers
     }
+  ],
+  plugins: [
+    sass({
+      injectGlobalPaths: [
+        'src/styles/global/_variables.scss'
+      ]
+    })
   ]
 };
